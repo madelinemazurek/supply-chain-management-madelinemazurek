2e000996dfdee78f8106bb825b7a5537c7d3f7de
Dacard45 <81481781+Dacard45@users.noreply.github.com>
2021-03-27 14:29:08 -0600

    Update DatabaseAccess.java

--- edu/ucalgary/ensf409/DatabaseAccess.java
+++ edu/ucalgary/ensf409/DatabaseAccess.java
@@ -1,10 +1,12 @@
 package edu.ucalgary.ensf409;
 import java.sql.*;
+import java.util.*;
 public class DatabaseAccess{
     public final String DBURL;
     public final String USERNAME;
     public final String PASSWORD;
     private Connection dbConnect;
+    private ResultSet results;
     public DatabaseAccess(String dburl, String username, String password){
         this.DBURL = dburl;
         this.USERNAME = username;
@@ -29,7 +31,24 @@ public class DatabaseAccess{
             System.err.println("Couldn't execute query");
         }
     }
-    
+    public ArrayList<String> fetchTables(){
+        ArrayList <String> tableString = new ArrayList<String>();
+        try{
+            Statement myStmt = dbConnect.createStatement();
+            String query = "SHOW TABLES";
+            results = myStmt.executeQuery(query);
+            while(results.next()){
+                System.out.println(results.getString("TABLE_NAME"));
+            }
+        } catch (SQLException e){
+            System.err.print("Could not execute query.");
+        }
+        return tableString;
+    }
+    public String[] fetchTypes(String table){
+        String [] temp ={"Cool"};
+        return temp;
+    }
     public void close(){
         try{
             dbConnect.close();
@@ -37,5 +56,10 @@ public class DatabaseAccess{
         catch (SQLException e){
             System.err.println("Could not fully close");
         }
+    }
+    public static void main(String[] args){
+        DatabaseAccess myTest = new DatabaseAccess("jdbc:mysql://localhost/inventory","ethan", "Roxanne3");
+        myTest.initializeConnection();
+        ArrayList<String> cool = myTest.fetchTables();
     }
 }
