894d7333056ff3f090d14e4651c6132347f97b15
Tyler Thain <tyler.thain@ucalgary.ca>
2021-03-27 13:56:49 -0600

    Updated WriteText.java

--- edu/ucalgary/ensf409/WriteText.java
+++ edu/ucalgary/ensf409/WriteText.java
@@ -4,12 +4,18 @@ import java.io.*;
 public class WriteText{
     private BufferedWriter writer;
     private String fileName = "orderform.txt";
+    private String category;
+    private String type;
+    private String number;
     private String[] itemIDs;
     private String price;
     private boolean canComplete;
     private String output;
 
-    public WriteText(String[] itemIDs, String price, boolean canComplete){
+    public WriteText(String category, String type, String number, String[] itemIDs, String price, boolean canComplete){
+        this.category = category;
+        this.type = type;
+        this.number = number;
         this.itemIDs = itemIDs;
         this.price = price;
         this.canComplete = canComplete;
@@ -28,10 +34,55 @@ public class WriteText{
         if(canComplete){
             formatOutputComplete();
         }
+        else{
+            formatOutputCannotComplete();
+        }
+
+        try{
+            writer.write(this.output);
+        }
+        catch(IOException e){
+            System.out.println("Unable to write to output file.");
+            e.printStackTrace();
+        }
+        finally{
+            try{
+                //try to close the file if the file was opened
+                if(writer != null){
+                    writer.close();
+                }
+            }
+            catch(IOException e){
+                //if file cannot be closed, exit
+                System.out.println("Error closing output file.");
+                e.printStackTrace();
+            }
+        }
+
     }
 
     private void formatOutputComplete(){
-        output = "Furniture Order Form";
+        this.output = "";
+        this.output = "Furniture Order Form\n";
+        this.output += "\n";
+        this.output += "Faculty Name:\n";
+        this.output += "Contact:\n";
+        this.output += "Date:\n";
+        this.output += "\n";
+        this.output +=  "Original Request: " + type + " " + category + ", " + number + "\n";
+        this.output += "\n";
+        this.output += "Items Ordered\n";
+        for(int i = 0; i < this.itemIDs.length; i++){
+            this.output += "ID: ";
+            this.output += this.itemIDs[i];
+            this.output += "\n";
+        }
+        this.output += "\n";
+        this.output += "Total Price: $" + price;
+    }
+
+    private void formatOutputCannotComplete(){
+
     }
 
 }
