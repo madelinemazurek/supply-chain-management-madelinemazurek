a424cc313124148a68532ea9c65a30da481cbcf5
Madeline <madeline.mazurek@ucalgary.ca>
2021-03-28 08:50:04 -0600

    added documentation for Lamp

--- edu/ucalgary/ensf409/Lamp.java
+++ edu/ucalgary/ensf409/Lamp.java
@@ -1,9 +1,34 @@
+/**
+*@author     Madeline Mazurek <a href="mailto:madeline.mazurek@ucalgary.ca">madeline.mazurek@ucalgary.ca</a> 
+*@author     Jared Assen <a href="mailto:jared.assen@ucalgary.ca">jared.assen@ucalgary.ca</a> 
+*@author     Ethan Card <a href="mailto:michael.card@ucalgary.ca">michael.card@ucalgary.ca</a> 
+*@author     Tyler Thain <a href="mailto:tyler.thain@ucalgary.ca">tyler.thain@ucalgary.ca</a> 
+*@version    1.1
+*@since      1.0
+*/
+
 package edu.ucalgary.ensf409;
 
+/**
+ * Lamp class denotes a lamp item which is read in and initialized from the database.
+ *  This contains fields for all of the specific components of a lamp item
+ *  (Base, Bulb), which are used to make a new recycled lamp.
+ */
 public class Lamp extends FurnitureItem {
-    private final char base;
-    private final char bulb;
+    private final char base; //A character boolean (Y/N) for whether or not this lamp has an avaliable Base
+    private final char bulb; //A character boolean (Y/N) for whether or not this lamp has an avaliable Bulb
 
+    /**
+     * Lamp constructor. Takes in parameters for all fields and initializes 
+     *  the member variables. Uses a super() constuctor call to initialize the fields in
+     *  FurnitureItem class.
+     * @param id The ID number for a furniture item (ex: C0914).
+     * @param type The Type for a furniture item (ex: Mesh for a chair).
+     * @param base A character boolean (Y/N) for whether or not this lamp has an avaliable Base.
+     * @param bulb A character boolean (Y/N) for whether or not this lamp has an avaliable Bulb.
+     * @param price The Price for a furniture item (ex: 50 ($)).
+     * @param manuId The Manufacturer ID number for a furniture item (ex: 002).
+     */
     public Lamp (String id, String type, 
                 char base, char bulb,
                 int price, String manuId) {
@@ -12,10 +37,18 @@ public class Lamp extends FurnitureItem {
         this.bulb = bulb;
     }
 
+    /**
+     * Getter for Base field.
+     * @return A character boolean (Y/N) for whether or not this lamp has an avaliable Base.
+     */
     public char getBase() {
         return base;
     }
 
+    /**
+     * Getter for Bulb field.
+     * @return A character boolean (Y/N) for whether or not this lamp has an avaliable Bulb.
+     */
     public char getBulb() {
         return bulb;
     }
