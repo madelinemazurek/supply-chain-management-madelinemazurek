f2563f6b7865917009c44bd1d8fe89bcf169a917
Tyler Thain <tyler.thain@ucalgary.ca>
2021-03-27 21:41:05 -0600

    Update to WriteText

--- edu/ucalgary/ensf409/WriteText.java
+++ edu/ucalgary/ensf409/WriteText.java
@@ -1,9 +1,15 @@
 package edu.ucalgary.ensf409;
 import java.io.*;
 
+/**
+ * WriteText creates an output file for the orderform that corresponds to
+ * the furniture items that are being purchased. It uses the furniture
+ * category, type, and number, as well as the furniture ID of the used items
+ * being purchased, and the overall purchase price.
+ */
 public class WriteText{
     private BufferedWriter writer;
-    private String fileName = "orderform.txt";
+    private String fileName = "orderform.txt";  //hardcoding filename
     private String category;
     private String type;
     private String number;
@@ -19,19 +25,23 @@ public class WriteText{
         this.price = price;
     }
 
-    private void writeOutput(){
-        File out;
+    public void writeOutput(){
+        File out;   //declare a file object
         try{
+            //instantiate file object with fileName
             out = new File(fileName);
+            //instantiate a BufferedWriter object using the File object
             writer = new BufferedWriter(new FileWriter(out));
         }
         catch(IOException e){
             System.out.println("Error opening output file.");
             e.printStackTrace();
         }
-        formatOutputComplete();
+        //format the output string
+        formatOutput();
 
         try{
+            //write the output string to the file
             writer.write(this.output);
         }
         catch(IOException e){
@@ -54,7 +64,8 @@ public class WriteText{
 
     }
 
-    private void formatOutputComplete(){
+    private void formatOutput(){
+        //format the string to contain the appropriate characters and newlines
         this.output = "";
         this.output = "Furniture Order Form\n";
         this.output += "\n";
@@ -65,6 +76,7 @@ public class WriteText{
         this.output +=  "Original Request: " + type + " " + category + ", " + number + "\n";
         this.output += "\n";
         this.output += "Items Ordered\n";
+        //iterate through the itemIDs array and insert them into the string
         for(int i = 0; i < this.itemIDs.length; i++){
             this.output += "ID: ";
             this.output += this.itemIDs[i];
